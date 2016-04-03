8052466fce5466428d26591bbae3f24588735564
rocketNeck <homestarflyer@gmail.com>
2016-04-03 01:46:10 -0400

    getting there
    Refactoring a little as i go.
    Used my notes to thy to create as many functioning routes as possible.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,95 +1,145 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    enable :sessions
-    set :session_secret, "password_security"
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
   end
 
   get '/' do
     erb :index
   end
-####################### signup flow ##################
+  ####################### signup flow ##################
   get '/signup' do
-    if !logged_in?
-      erb :'users/create_user'
+    if logged_in?
+      redirect '/tweets'
     else
-      redirect to "/tweets"
+      erb :'/users/create_user'
     end
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == "" # && EMAIL_REGEX.match(params[:email]) check if lagit email
+    if params.any? { |k, v| v.empty? }
       redirect '/signup'
     else
-      @user = User.create(username: params[:username],email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-      redirect "/tweets"
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect '/tweets'
     end
   end
-
-
-
-################# log in flow ###################
+  ################# log in flow ###################
   get '/login' do
-   erb :'users/login'
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
   end
 
   post '/login' do
-    @user = User.find(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "/tweets/tweets"
-    else
-      redirect to "/signup"
-    end
+    @user = User.find_by(username: params[:username])
+  #  if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect to '/tweets'
+  #  else
+    #  redirect to '/signup'
+  #  end
   end
-###########################################################
+  ###########################################################
+
   get '/tweets' do
+    @tweets = Tweet.all
     if logged_in?
-      @tweets = Tweet.all
-      erb :'/tweets/tweets'
+      erb :'/tweets'
     else
       redirect '/login'
     end
   end
-                 #must check password and set session
 
-  #after logged in loads tweets.erb
+  get '/logout' do
+    session.clear if logged_in?
+    redirect '/login'
+  end
 
-  #logout will remove the session hash adn direct back to welcome page
-            #you can't log out if you are not logged in
-            #can not see tweets if you are not logged in
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
-  #you can see all tweets by a selected user at /users/#{user.slug} view
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
 
-  #have a link on the tweets.erb page to link to a create new tweet page  '/tweets/new'
-  #after linking to the ('/tweets/new' renders create_tweet.erb) page you see a form to create a tweet or link back home
-                         #//////////(maybe have a nav on every page to link around)
-                         #you can only see the create tweet page if you are logged in
-                         #creating a Tweet assigns that tweet to the User that created it
-                         #once the form for the new tweet is submitted you are redirected.... to? you can not create a blank tweet
+  post '/tweets/new' do
+    @tweet = Tweet.create(content: params[:content],
+                          user_id: session[:id])
+    if @tweet.content.empty?
+      redirect '/tweets/new'
+    else
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
 
-  #see individual tweets with /tweets/:id renders show_tweets.erb
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+  # after linking to the ('/tweets/new' renders create_tweet.erb) page you see a form to create a tweet or link back home
+  # //////////(maybe have a nav on every page to link around)
+  # you can only see the create tweet page if you are logged in
+  # creating a Tweet assigns that tweet to the User that created it
+  # once the form for the new tweet is submitted you are redirected.... to? you can not create a blank tweet
 
-  #there is a link here to allow you to edit   href="tweets/:id/edit"
-             #a user can only submit edit  IF they are the tweet creator
-             #when you edit a tweet you can not submit it without and contend     :content != ""
-            #edit page is /tweets/:id/edit  it renders edit_tweet.erb once the edit form is submited you are redirected....?
+  # see individual tweets with /tweets/:id renders show_tweets.erb
 
-  #need a "Delete Tweet" button on the /tweet/:id   erb :tweets page (uses a hidden for "DELETE" request)
-                #checks if the user is the tweet creator by checking the tweet forgin key
+  # there is a link here to allow you to edit   href="tweets/:id/edit"
+  # a user can only submit edit  IF they are the tweet creator
+  # when you edit a tweet you can not submit it without and contend     :content != ""
+  # edit page is /tweets/:id/edit  it renders edit_tweet.erb once the edit form is submited you are redirected....?
+  get '/tweet/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in?
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
 
+  patch '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+    if @tweet.contend != ''
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
 
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete if session[:id] == @tweet.user_id
+    redirect '/tweets'
+  end
+  # need a "Delete Tweet" button on the /tweet/:id   erb :tweets page (uses a hidden for "DELETE" request)
+  # checks if the user is the tweet creator by checking the tweet forgin key
   helpers do
     def logged_in?
-      !!session[:user_id]
+      !!session[:id]
     end
+
     def current_user
-      User.find(session[:user_id])
+      User.find(session[:id])
     end
   end
 end
